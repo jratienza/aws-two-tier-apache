a8f5f3db1d2f87228a5012e21f1665a0d1aad58a
johnashly.atienza <johnashly.atienza@eee.upd.edu.ph>
2020-12-12 13:50:59 +0800

    change to instance private ip

--- outputs.tf
+++ outputs.tf
@@ -3,11 +3,11 @@ output "jumphost_public_ip"{
 }
 
 output "apache1_private_ip"{
-    value = aws_instance.apache1.public_ip
+    value = aws_instance.apache1.private_ip
 }
 
 output "apache2_public_ip"{
-    value = aws_instance.apache2.public_ip
+    value = aws_instance.apache2.private_ip
 }
 
 output "alb_address" {
